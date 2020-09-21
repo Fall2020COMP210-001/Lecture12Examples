7872bb9c93e2ba46520b0a1d70759fa5349fbcc2
Ketan Mayer-Patel <kmp@cs.unc.edu>
2020-09-21 19:50:09 -0400

    Init

---
+++ src/lec12/BinaryTreeImpl.java
@@ -0,0 +1,60 @@
+package lec12;
+
+public class BinaryTreeImpl<T> implements BinaryTree<T> {
+
+    private T _value;
+    private BinaryTree<T> _left;
+    private BinaryTree<T> _right;
+
+    public BinaryTreeImpl(T value, BinaryTree<T> left, BinaryTree<T> right) {
+        _value = value;
+        _left = left;
+        _right = right;
+    }
+
+
+    @Override
+    public T getValue() {
+        return _value;
+    }
+
+    @Override
+    public void setValue(T value) {
+        _value = value;
+    }
+
+    @Override
+    public BinaryTree<T> getLeft() {
+        return _left;
+    }
+
+    @Override
+    public BinaryTree<T> getRight() {
+        return _right;
+    }
+
+    @Override
+    public void setLeft(BinaryTree<T> left) {
+        _left = left;
+    }
+
+    @Override
+    public void setRight(BinaryTree<T> right) {
+        _right = right;
+    }
+
+    @Override
+    public int height() {
+        // Done in class
+    }
+
+    @Override
+    public boolean contains(T value) {
+        // Done in class
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // Done in class
+    }
+}
